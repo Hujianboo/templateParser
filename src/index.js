c576ddac4b56eb3d470e4653aabea338cd0093b4
hujianbo <466636709@qq.com>
2020-09-14 15:31:27 +0800

    add if else

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 // 模板引擎第一版
 (function() {
     this.tmpl = function (str) {
-
     	var str = document.getElementById(str).innerHTML;
-
     	var stringBody = "let p = []; p.push('" +
     	str.replace(/[\r\n\t]/g,'')
         .replace(/<%=(.*?)%>/g, "');p.push($1);p.push('")
         .replace(/<%/g,"');")
         .replace(/%>/g,"p.push('") + "');return p.join('');";
         console.log(stringBody);
-        let fn = new Function('obj',stringBody);
-        return fn();
-
-    	// return p.join('');
+        let fn = new Function(stringBody);
+        return function(){
+            return fn.call(this);
+        }
     };
 })();
 
@@ -25,4 +23,4 @@ var users = [
         { "name": "Frank", "url": "http://localhost" }
     ]
 
-results.innerHTML = tmpl("user_tmpl", users);
+results.innerHTML = tmpl("user_tmpl")();
