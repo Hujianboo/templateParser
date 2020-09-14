36a5c7b87aaf51b95546fdbc8f024fdfe7ce268e
hujianbo <466636709@qq.com>
2020-09-14 11:09:48 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// 模板引擎第一版
+(function() {
+    this.tmpl = function (str) {
+
+    	var str = document.getElementById(str).innerHTML;
+
+    	var stringBody = "let p = []; p.push('" +
+    	str.replace(/[\r\n\t]/g,'')
+        .replace(/<%=(.*?)%>/g, "');p.push($1);p.push('")
+        .replace(/<%/g,"');")
+        .replace(/%>/g,"p.push('") + "');return p.join('');";
+        console.log(stringBody);
+        let fn = new Function('obj',stringBody);
+        return fn();
+
+    	// return p.join('');
+    };
+})();
+
+var results = document.getElementById("container");
+
+var users = [
+        { "name": "Byron", "url": "http://localhost" },
+        { "name": "Casper", "url": "http://localhost" },
+        { "name": "Frank", "url": "http://localhost" }
+    ]
+
+results.innerHTML = tmpl("user_tmpl", users);
